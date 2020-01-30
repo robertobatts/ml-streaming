e05044ec06631c837663805a3212df601cc823e7
Roberto Battaglia <battaroberto@gmail.com>
2020-01-30 20:30:01 +0000

    sql

---
+++ sql/filterbytimedifference.sql
@@ -0,0 +1,6 @@
+SELECT C1.deviceId, C1.latitude, C1.longitude, C1.url, C1.timestamp
+  FROM CameraInput C1 TIMESTAMP BY timestamp
+  JOIN CameraInput C2 TIMESTAMP BY timestamp
+    ON C1.deviceId = C2.deviceId
+        AND DATEDIFF(ss, C1, C2) BETWEEN 0 AND 10
+        AND C1.timestamp != C2.timestamp
